88a62ebb9a24321bdc98bdf6d20b9732fd4ea83a
fbottazzi <francobottazzi2014@gmail.com>
2024-06-26 14:46:19 -0300

    First commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("Hallo Welt\n");
+
+    for (int i = 0; i < 20; i++)
+    {
+        printf("\na");
+    }
+    printf("%%\n");
+    
+    
+    
+    return 0;
+}
